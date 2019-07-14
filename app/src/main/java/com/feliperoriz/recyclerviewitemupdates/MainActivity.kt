bc0cfe3589c65811f110f9951c98b6489fcede19
Felipe Roriz <froriz50@gmail.com>
2019-07-14 16:21:14 -0500

    Added tablayout with fragments

--- app/src/main/java/com/feliperoriz/recyclerviewitemupdates/MainActivity.kt
+++ app/src/main/java/com/feliperoriz/recyclerviewitemupdates/MainActivity.kt
@@ -3,10 +3,19 @@ package com.feliperoriz.recyclerviewitemupdates
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupTabLayout()
+    }
+
+    private fun setupTabLayout() {
+        view_pager.adapter = FragmentPagerAdapter(supportFragmentManager)
+        tab_layout.setupWithViewPager(view_pager)
     }
 }
